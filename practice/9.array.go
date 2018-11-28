bf2dab38883655cf04db9e2e21313880897459b5
kcboi95 <kcboi95@gmail.com>
2018-11-28 23:39:17 +0700

    Array update

--- practice/9.array.go
+++ practice/9.array.go
@@ -7,12 +7,14 @@ import (
 // Array - how to use
 func Array() {
 	var names [5]string
-
 	names[0] = "Dat"
 	names[1] = "John"
 	names[2] = "Krista"
 	names[3] = "Lorem"
 	names[4] = "Wowy"
+
+	// Fast way
+	//names := [5]string{"Dat","John","Krista","Lorem","Wowy"}
 
 	fmt.Println("------------------------------------------------------------")
 	fmt.Println("|                         ARRAY                            |")
