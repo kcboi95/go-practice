966369c9e1731e466ef3ae06dffcb92683dd65a1
kcboi95 <kcboi95@gmail.com>
2018-11-28 19:45:03 +0700

    Make useful menu instead of run all

--- main.go
+++ main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"go-practice/practice"
 )
 
 func main() {
-	practice.HelloWorld()
-	practice.DataTypes()
-	practice.Variables()
-	practice.Constants()
-	practice.DecisionMaking()
-	practice.Loop()
+	for true {
+		fmt.Println("------------------------------------------------------------")
+		fmt.Println("| Plese choose one value in the list below                 |")
+		fmt.Println("| to see the result or use `Ctrl + C` to exit the program  |")
+		fmt.Println("------------------------------------------------------------")
+		fmt.Println("1 . Hello World")
+		fmt.Println("2 . Data types")
+		fmt.Println("3 . Variables")
+		fmt.Println("4 . Constants")
+		fmt.Println("5 . Decision making")
+		fmt.Println("6 . Loop")
+		fmt.Print("Value:")
+
+		var value int
+		_, err := fmt.Scanf("%d\n", &value)
+
+		// Capture error
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		switch value {
+		case 1:
+			practice.HelloWorld()
+
+		case 2:
+			practice.DataTypes()
+
+		case 3:
+			practice.Variables()
+
+		case 4:
+			practice.Constants()
+
+		case 5:
+			practice.DecisionMaking()
+
+		case 6:
+			practice.Loop()
+	}
 }
